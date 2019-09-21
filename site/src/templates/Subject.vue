dd7179ebe567a687105136b97d0aab9f8e84882b
やま <40002825+koorimizuw@users.noreply.github.com>
2019-09-21 23:18:50 +0900

    Add files via upload

--- site/src/templates/Subject.vue
+++ site/src/templates/Subject.vue
@@ -1,144 +1,97 @@
 <template>
-  <syllabus-layout class="subject">
-    <h1 class="syllabus-page-title">{{ $page.subject.title }}</h1>
-    <p class="subject-teacher">
-      <g-link :to="`/teacher/${$page.subject.teacher.id}`">
-        {{
-        $page.subject.teacher.name
-        }}
-      </g-link>
-    </p>
-
-    <div class="syllabus-page-statistics-wrapper">
-      <h2 class="syllabus-page-statistics-title subject-margin">Tags</h2>
-
-      <syllabus-button :link="link" v-for="(link,id) in tags" :key="id" />
-
-      <syllabus-section title="Purpose" :content="detail.purpose" />
-      <syllabus-section title="Target" :content="detail.target" />
-      <syllabus-section title="Teaching Style" :content="detail.teachingStyle" />
-      <syllabus-section title="Grade Policy" :content="detail.gradePolicy" />
-      <syllabus-section title="Final Test" :content="detail.finalTest" />
-      <syllabus-section title="Message" :content="detail.message" />
-      <syllabus-section title="Keywords" :content="detail.keywords" />
-      <syllabus-section title="Contents" :content="detail.contents" />
-    </div>
+  <syllabus-layout>
+    <v-card width="1500" class="mx-auto subject-title">
+      <v-card-title>
+        All
+        <span class="subject-length">{{ subjects.length }}</span>
+        Subjects
+        <v-spacer />
+        <v-text-field v-model="searchText" append-icon="search" label="講義名検索"></v-text-field>
+      </v-card-title>
+      <syllabus-card :links="subjects" :card_col="3" />
+    </v-card>
   </syllabus-layout>
+  <!--
+  <syllabus-layout>
+    <h2 class="syllabus-page-title">
+      All
+      <span class="syllabus-page-title-count">{{ subjects.length }}</span>
+      Subjects
+    </h2>
+
+    <input
+      class="syllabus-input"
+      placeholder="講義名検索"
+      v-model="searchText"
+    />
+
+    <transition-group name="syllabus-tags" tag="nav">
+      <syllabus-button :link="link" v-for="link in subjects" :key="link.id" />
+    </transition-group>
+  </syllabus-layout>
+  -->
 </template>
 
+<script>
+import SyllabusCard from "../components/SyllabusCard";
+
+export default {
+  metaInfo: {
+    title: "Subjects"
+  },
+  data() {
+    return {
+      page_col: 3,
+      searchText: ""
+    };
+  },
+  computed: {
+    subjects() {
+      let subjects = this.$page.allSubject.edges.map(edge => {
+        const { title, id } = edge.node;
+        return {
+          id,
+          name: title,
+          url: `/subject/${id}`
+        };
+      });
+
+      if (this.searchText) {
+        subjects = subjects.filter(subject =>
+          subject.name.includes(this.searchText)
+        );
+      }
+      return subjects;
+    }
+  },
+  components: {
+    SyllabusCard
+  }
+};
+</script>
+
 <page-query>
-query Subject($id: String!) {
-  subject: subject(id: $id) {
-    title
-    subtitle
-    generalTitle
-    time
-    location
-    type
-    category {
-      id
-      name
-    }
-    field {
-      name
-      id
-    }
-    year {
-      name
-    }
-    compulsory
-    detail {
-      purpose
-      target
-      message
-      keywords
-      contents
-      finalTest
-      teachingStyle
-      gradePolicy
-      department
-    }
-    teacher {
-      id
-      name
+query {
+  allSubject {
+    edges {
+      node {
+        id
+        title
+      }
     }
   }
 }
 </page-query>
 
-<script>
-export default {
-  metaInfo() {
-    return {
-      title: this.$page.subject.title
-    };
-  },
-  computed: {
-    tags() {
-      const subject = this.$page.subject;
-      return [
-        {
-          name: subject.time,
-          url: `#`
-        },
-        {
-          name: subject.location,
-          url: `#`
-        },
-        {
-          name: subject.type,
-          url: `#`
-        },
-        {
-          name: subject.category.name,
-          url: `/category/${subject.category.id}`
-        },
-        {
-          name: subject.field.name,
-          url: `/field/${subject.field.id}`
-        },
-        {
-          name:
-            subject.year.name === "不明"
-              ? "年: 不明"
-              : subject.year.name + "年",
-          url: `#`
-        },
-        {
-          name: subject.compulsory,
-          url: `#`
-        }
-      ].filter(tag => tag.name && tag.name !== "不明");
-    },
-    detail() {
-      return this.$page.subject.detail;
-    }
-  }
-};
-</script>
-
 <style lang="scss" scoped>
 .subject {
   &-title {
-    &-sub {
-      text-align: center;
-      color: $theme-green;
-      font-size: 1.4rem;
-    }
+    padding: 30px 40px;
   }
-
-  &-teacher {
-    font-size: 1.6rem;
-    color: $theme-grey;
-    text-align: center;
-  }
-}
-@media screen and (max-width: 640px) {
-  .subject {
-    &-txt {
-      padding-left: 0;
-      font-size: 1.1rem;
-    }
+  &-length {
+    margin: 5px;
+    font-size: 1.2em;
+    color: rgb(0, 147, 226);
   }
 }
 </style>
