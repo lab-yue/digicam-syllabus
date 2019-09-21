d445fe3e8c06e5bb0d08141f60d95c25cd2fd340
やま <40002825+koorimizuw@users.noreply.github.com>
2019-09-21 19:19:00 +0900

    Add files via upload

---
+++ site/src/components/SyllabusCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="pa-2" fluid>
+    <v-row>
+      <template v-for="n in card_col">
+        <v-col :key="n">
+          <v-list>
+            <template v-for="(link, index) in links">
+              <g-link v-if="index%card_col===n-1" :key="link.id" :to="link.url">
+                <v-list-item @click>
+                  <v-list-item-content>
+                    <v-list-item-title v-text="link.name"></v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </g-link>
+            </template>
+          </v-list>
+        </v-col>
+        <v-divider v-if="n % card_col != 0 && n < links.length" vertical></v-divider>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["links", "card_col"]
+};
+</script>
