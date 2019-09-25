ff1c1fdc5a93bcc27bb95f79fa6e0b2b4f6e7cca
やま <40002825+koorimizuw@users.noreply.github.com>
2019-09-26 07:40:45 +0900

    Add files via upload

--- site/src/pages/Teacher.vue
+++ site/src/pages/Teacher.vue
@@ -8,6 +8,16 @@
         <v-spacer />
         <v-text-field v-model="searchText" append-icon="search" label="名前検索"></v-text-field>
       </v-card-title>
+      <v-chip
+        v-for="position in positions"
+        :key="position.name"
+        @click.native="() => setPosition(position.name)"
+        class="ma-2"
+        color="primary"
+        link
+        :outlined="searchPosition !== position.name"
+        pill
+      >{{position.name}}</v-chip>
       <syllabus-card :links="teachers" :card_col="4" />
     </v-card>
   </syllabus-layout>
@@ -20,13 +30,30 @@
 
     <input class="syllabus-input" placeholder="名前検索" v-model="searchText" />
 
+    <h2 class="syllabus-page-statistics-title">Positions</h2>
+
+    <transition-group name="syllabus-tags" tag="nav">
+      <syllabus-button
+        v-for="position in positions"
+        :class="[
+          'position',
+          (searchPosition && (position.name !== searchPosition)) 
+          ? 'position-inactive'
+          : 'position-active'
+        ]"
+        :link="position"
+        :key="position.name"
+        @click.native="() => setPosition(position.name)"
+      />
+    </transition-group>
+
+    <h2 class="syllabus-page-statistics-title subject-margin">Teachers</h2>
     <transition-group name="syllabus-tags" tag="nav">
       <syllabus-button :link="link" v-for="link in teachers" :key="link.name" />
     </transition-group>
   </syllabus-layout>
   -->
 </template>
-
 <script>
 import SyllabusCard from "../components/SyllabusCard";
 
@@ -36,27 +63,53 @@ export default {
   },
   data() {
     return {
-      page_col: 4,
-      searchText: ""
+      searchText: "",
+      searchPosition: ""
     };
   },
   computed: {
     teachers() {
       let teachers = this.$page.allTeacher.edges.map(edge => {
-        const { name, id, subjects } = edge.node;
+        const { name, id, subjects, position } = edge.node;
         return {
           extra: subjects.totalCount,
           name,
+          position: position.trim() || "不明",
           url: `/teacher/${id}`
         };
       });
 
-      if (this.searchText) {
-        teachers = teachers.filter(teacher =>
-          teacher.name.includes(this.searchText)
-        );
-      }
+      teachers = teachers.filter(teacher => {
+        if (this.searchText && !teacher.name.includes(this.searchText)) {
+          return false;
+        }
+        if (this.searchPosition && teacher.position !== this.searchPosition) {
+          return false;
+        }
+        return true;
+      });
+
       return teachers;
+    },
+    positions() {
+      let count = {};
+      let positions = this.$page.allTeacher.edges.map(edge => {
+        const position = edge.node.position.trim() || "不明";
+
+        if (position) {
+          count[position] = (count[position] || 0) + 1;
+        }
+      });
+      return Object.entries(count).map(([k, v]) => ({
+        name: k,
+        extra: v,
+        url: "#"
+      }));
+    }
+  },
+  methods: {
+    setPosition(p) {
+      this.searchPosition = this.searchPosition === p ? "" : p;
     }
   },
   components: {
@@ -72,6 +125,7 @@ query {
       node {
         id
         name
+        position
         subjects {
           totalCount
         }
