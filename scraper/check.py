fa6adfcbea46186fdaad92d842ee21f78e338a29
rainy-me <github@rainy.me>
2019-09-28 13:18:28 +0900

    update checker

--- scraper/check.py
+++ scraper/check.py
@@ -1,29 +1,52 @@
+import asyncio
+import aiohttp
+
 import sanitizer
-import json
-from main import setup, SAVE_DATA_FILE
+import store
+from main import setup
+from constants import SYLLABUS_URL
 
 
 def get_local_len():
-    local_data = None
-    with open(SAVE_DATA_FILE) as f:
-        local_data = json.load(f)
-    return len(local_data['data'])
+    db = store.Instance()
+    db.load()
+    return len(db.data)
 
 
-def check_len():
+async def check_len():
     print('=========  check! =========')
     local_len = get_local_len()
-    _, page, _ = setup(skip_page_data=True)
-    web_len = int(sanitizer.get_syllabus_count(page['text']))
+
+    s = aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(
+            ssl=False,
+            limit=50,
+            force_close=True
+        ))
+
+    async with s.get(SYLLABUS_URL) as res:
+        viewstate = sanitizer.get_viewstate(await res.text())
+        form_data = {
+            '__VIEWSTATE': viewstate,
+            'btnSearch': '以上の条件で検索'
+        }
+
+    async with s.post(SYLLABUS_URL, data=form_data) as res:
+        text = await res.text()
+        web_len = int(sanitizer.get_syllabus_count(text))
+
+    await s.close()
+
     is_equal = local_len == web_len
     message = 'same.' if is_equal else 'should update.'
+
     print(f'''
     local_len, {local_len}
     web_len, {web_len}
     {message}
     ''')
+
     return is_equal
 
-
 if __name__ == '__main__':
-    check_len()
+    asyncio.get_event_loop().run_until_complete(check_len())
